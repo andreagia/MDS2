40d64f0090f2a0262a006ac2a1106cd762d926e8
andrea <andreagia71@gmail.com>
2017-01-11 11:47:55 +0100

    First submition

---
+++ src/main/java/com.cirmmp.mds2.controller/Access.java
@@ -0,0 +1,23 @@
+package com.cirmmp.mds2.controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+
+@Controller
+@RequestMapping("/welcome")
+public class Access  {
+
+
+    @RequestMapping(method = RequestMethod.GET)
+    public ModelAndView helloWorld(){
+
+        ModelAndView model = new ModelAndView("HelloWorldPage");
+        model.addObject("msg", "hello world");
+
+        return model;
+    }
+}
---
+++ src/main/webapp/WEB-INF/jsp/helloworld.jsp
@@ -0,0 +1,15 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: andrea
+  Date: 10/01/2017
+  Time: 16:36
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
+<html>
+<body>
+<h1>Spring MVC Hello World Example</h1>
+
+<h2>${msg}</h2>
+</body>
+</html>
