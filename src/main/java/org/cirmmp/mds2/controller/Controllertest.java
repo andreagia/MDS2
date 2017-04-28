774404fcc01542ed866363a743a0bf8d93237bfa
andrea <andreagia71@gmail.com>
2017-04-28 09:43:59 +0200

    jetty https jetty 9.3 with java 8

--- src/main/java/org/cirmmp/mds2/controller/Controllertest.java
+++ src/main/java/org/cirmmp/mds2/controller/Controllertest.java
@@ -4,11 +4,13 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  * Created by andrea on 27/04/2017.
  */
 @Controller
+@RestController
 public class Controllertest {
 
     @CrossOrigin
